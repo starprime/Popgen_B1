9f185ccab17a56c4e10f24b686f6d8a285164840
starprime <skuma115@asu.edu>
2017-09-23 22:02:53 -0700

    change

--- EnterPage/admin.py
+++ EnterPage/admin.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from models import User,Job
+from .models import User,Job
 
 admin.site.register(User)
 admin.site.register(Job)
+
